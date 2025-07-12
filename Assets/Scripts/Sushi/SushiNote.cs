e7c8801d04207ae1437008f4c2740b95d8bd2225
ZUKO <elainechew01@gmail.com>
2025-07-13 01:22:01 +0800

    Sushi Edit 3.0

--- Assets/Scripts/Sushi/SushiNote.cs
+++ Assets/Scripts/Sushi/SushiNote.cs
@@ -1,6 +1,27 @@
 using UnityEngine;
 
-public class SushiNote
+public class SushiNote : Note
 {
-    
+    public override void Setup(Conductor conductor, Transform spawnPoint, Transform hitPoint, NoteData noteData, float approachRate)
+    {
+        base.Setup(conductor, spawnPoint, hitPoint, noteData, approachRate);
+
+        // You can also do custom spawn behavior here if needed
+    }
+
+    protected override void Update()
+    {
+        if (isInitialized)
+        {
+            float songTime = conductor.currentSongPosition;
+            float timeRemaining = hitTiming - songTime;
+            float normalizedTime = 1f - (timeRemaining / approachRate);
+            // Don't clamp normalizedTime
+
+            Vector3 rightMovement = new Vector3(normalizedTime * 2f, 0f, 0f);
+            transform.position = spawnPoint.position + rightMovement;
+        }
+    }
 }
+
+
--- Assets/Scripts/Sushi/SushiScript.cs
+++ Assets/Scripts/Sushi/SushiScript.cs
@@ -3,41 +3,50 @@ using System.Collections.Generic;
 using System.Collections;
 using TMPro;
 using UnityEngine.SceneManagement;
+
 public class SushiScript : MonoBehaviour
 {
-    //song related
+    // Song setup
     public Conductor conductor;
     private float currentSongPosition;
-    [SerializeField] int bpm;
+    [SerializeField] private int bpm;
     [SerializeField] private AudioClip song;
     public List<NoteData> noteDataList = new();
 
-    //score windows
-    [SerializeField] private float perfectWindow;
-    [SerializeField] private float missWindow;
+    // Scoring
+    [SerializeField] private float perfectWindow = 0.1f;
+    //[SerializeField] private float goodWindow = 0.25f; // Optional
+    [SerializeField] private float missWindow = 0.5f;
     [SerializeField] private TouchManager touchManager;
 
-    //debug remvoe later
+    // Note spawning
     [SerializeField] private Transform spawnPoint;
     [SerializeField] private Transform hitPoint;
-    [SerializeField] private TMP_Text scoreText;
-    [SerializeField] private Note tapNotePrefab;
+    [SerializeField] private SushiNote tapNotePrefab;
+    public float approachRate = 1f;
     public List<Note> activeNotesList = new();
-    public float approachRate;
 
-    //visuals
-    [SerializeField] Transform player;
-    [SerializeField] Sprite playerSprite1;
-    [SerializeField] Sprite playerSprite2;
+    // UI
+    [SerializeField] private TMP_Text scoreText;
+
+    // Player visuals
+    [SerializeField] private Transform player;
+    [SerializeField] private Sprite playerSprite1;
+    [SerializeField] private Sprite playerSprite2;
     private SpriteRenderer spriteRenderer;
+    Vector3 originalPos;
+    [SerializeField] private float bobAmount = 0.2f;
+
+    // Conveyor belts
     public Transform belt1;
     public Transform belt2;
     public float moveAmount = 1f;
     public float beltWidth;
     private int spawnIndex = 0;
     float hittiming = 0f;
-    Vector3 originalPos;
-    [SerializeField] private float bobAmount = 0.2f;
+
+    private float nextHalfBeat;
+    private float nextFullBeat;
 
     private void Start()
     {
@@ -57,7 +66,6 @@ public class SushiScript : MonoBehaviour
                 hitTiming = hittiming,
             });
         }
-        float lastBeat = conductor.currentSongPosition;
     }
 
     private void OnEnable()
@@ -76,15 +84,10 @@ public class SushiScript : MonoBehaviour
         touchManager.OnSwipeDown -= TouchManager_OnSwipeDown;
     }
 
-    float nextHalfBeat;
-    float nextFullBeat;
-
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.W))
-        {
             SceneManager.LoadScene("ZenYoungTest");
-        }
 
         if (conductor.currentSongPosition > nextHalfBeat + conductor.crotchet)
         {
@@ -100,15 +103,10 @@ public class SushiScript : MonoBehaviour
             belt2.position += Vector3.right * moveAmount;
 
             if (belt1.position.x >= beltWidth)
-            {
                 belt1.position = new Vector3(belt2.position.x - beltWidth, belt1.position.y, belt1.position.z);
-            }
 
             if (belt2.position.x >= beltWidth)
-            {
                 belt2.position = new Vector3(belt1.position.x - beltWidth, belt2.position.y, belt2.position.z);
-            }
-
         }
 
         ClearInactiveNotes();
@@ -127,35 +125,30 @@ public class SushiScript : MonoBehaviour
         player.localPosition = originalPos + Vector3.down * bobAmount;
         yield return new WaitForSeconds(0.2f);
         player.localPosition = originalPos;
-        yield return null;
     }
 
     public void SpawnNote(NoteData noteData)
     {
-        Note prefab = null;
-        if (noteData.type == NoteType.Tap)
+        SushiNote prefab = null;
+
+        if (noteData.type == NoteType.Tap || noteData.type == NoteType.SwipeUp)
         {
             prefab = tapNotePrefab;
         }
-        if (noteData.type == NoteType.SwipeUp)
-        {
-            prefab = tapNotePrefab;
-        }
+
         if (prefab != null)
         {
-            Note note = Instantiate(prefab);
+            SushiNote note = Instantiate(prefab);
             note.Setup(conductor, spawnPoint, hitPoint, noteData, approachRate);
             activeNotesList.Add(note);
         }
-
     }
 
-    void ClearInactiveNotes()
+    private void ClearInactiveNotes()
     {
         for (int i = activeNotesList.Count - 1; i >= 0; i--)
         {
             Note note = activeNotesList[i];
-
             if (note == null)
             {
                 activeNotesList.RemoveAt(i);
@@ -177,20 +170,22 @@ public class SushiScript : MonoBehaviour
     public Note GetClosestNote(NoteType type)
     {
         Note closestNote = null;
-        float closestHitTiming = float.MaxValue;
+        float closestAbsOffset = float.MaxValue;
+
         foreach (var note in activeNotesList)
         {
             if (note.type != type) continue;
-            float hitTimingOffset = Mathf.Abs(currentSongPosition - note.hitTiming);
 
-            if (hitTimingOffset < -perfectWindow) continue;
+            float offset = currentSongPosition - note.hitTiming;
+            float absOffset = Mathf.Abs(offset);
 
-            if (hitTimingOffset < closestHitTiming)
+            if (absOffset <= perfectWindow && absOffset < closestAbsOffset)
             {
-                closestHitTiming = hitTimingOffset;
+                closestAbsOffset = absOffset;
                 closestNote = note;
             }
         }
+
         return closestNote;
     }
 
@@ -199,37 +194,41 @@ public class SushiScript : MonoBehaviour
         Note closestNote = GetClosestNote(type);
         if (closestNote == null) return;
 
-        float hitTimingOffset = Mathf.Abs(currentSongPosition - closestNote.hitTiming);
-        if (hitTimingOffset <= perfectWindow)
+        float offset = currentSongPosition - closestNote.hitTiming;
+        float absOffset = Mathf.Abs(offset);
+
+        if (absOffset <= perfectWindow)
         {
             Debug.Log("PERFECT");
             scoreText.color = Color.green;
-            scoreText.text = ("PERFECT");
-            Destroy(closestNote.gameObject);
+            scoreText.text = "PERFECT";
         }
+        /* else if (absOffset <= goodWindow) // Optional
+        {
+            Debug.Log("GOOD");
+            scoreText.color = Color.yellow;
+            scoreText.text = "GOOD";
+        } */
         else
         {
             Debug.Log("MISS");
             scoreText.color = Color.red;
-            scoreText.text = ("MISS");
-            Destroy(closestNote.gameObject);
+            scoreText.text = "MISS";
         }
+
+        Destroy(closestNote.gameObject);
+        activeNotesList.Remove(closestNote);
     }
 
+    // Touch Input Events
     private void TouchManager_OnScreenTouched(object sender, System.EventArgs e)
     {
         Debug.Log("Touch detected");
-
         if (!touchManager.swipeJustDetected)
-        {
             CheckNoteHitTiming(NoteType.Tap);
-        }
     }
 
-    private void TouchManager_OnScreenReleased(object sender, System.EventArgs e)
-    {
-        //SpriteRenderer.sprite = defaultImage;
-    }
+    private void TouchManager_OnScreenReleased(object sender, System.EventArgs e) { }
 
     private void TouchManager_OnSwipeUp(object sender, System.EventArgs e)
     {
@@ -240,7 +239,4 @@ public class SushiScript : MonoBehaviour
     {
         CheckNoteHitTiming(NoteType.SwipeDown);
     }
-
 }
-
-
