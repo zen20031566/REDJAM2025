1c942ae2ddb8520e5a25b0a188aebe979819b373
ZUKO <elainechew01@gmail.com>
2025-07-13 01:46:02 +0800

    Sushi Edit 5.0

--- Assets/Scripts/Sushi/SushiNote.cs
+++ Assets/Scripts/Sushi/SushiNote.cs
@@ -2,26 +2,40 @@ using UnityEngine;
 
 public class SushiNote : Note
 {
+    private bool hasBeenHit = false;
+    private float speed;
+
+    public bool HasBeenHit => hasBeenHit;
+
     public override void Setup(Conductor conductor, Transform spawnPoint, Transform hitPoint, NoteData noteData, float approachRate)
     {
         base.Setup(conductor, spawnPoint, hitPoint, noteData, approachRate);
-
-        // You can also do custom spawn behavior here if needed
+        speed = Vector3.Distance(spawnPoint.position, hitPoint.position) / approachRate;
     }
 
     protected override void Update()
     {
-        if (isInitialized)
-        {
-            float songTime = conductor.currentSongPosition;
-            float timeRemaining = hitTiming - songTime;
-            float normalizedTime = 1f - (timeRemaining / approachRate);
-            // Don't clamp normalizedTime
+        if (!isInitialized || hasBeenHit) return;
 
-            Vector3 rightMovement = new Vector3(normalizedTime * 2f, 0f, 0f);
-            transform.position = spawnPoint.position + rightMovement;
-        }
+        float songTime = conductor.currentSongPosition;
+        float timeSinceSpawn = songTime - (hitTiming - approachRate);
+        float distance = timeSinceSpawn * speed;
+
+        transform.position = spawnPoint.position + new Vector3(distance, 0f, 0f);
+    }
+
+    public void MarkAsHit()
+    {
+        hasBeenHit = true;
+        Destroy(gameObject); // or play hit animation
+    }
+
+    public void MarkAsMissed()
+    {
+        hasBeenHit = true;
+        Destroy(gameObject); // or play miss animation
     }
 }
+
 
 
